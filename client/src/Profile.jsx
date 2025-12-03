aa8cef7192247397b068fd10bfbe39229a8ddd9b
shubh <shubhamwagh2544@gmail.com>
2025-12-03 15:30:45 +0530

    client app sign-out added

--- client/src/Profile.jsx
+++ client/src/Profile.jsx
@@ -1,10 +1,12 @@
 import { useEffect, useState } from "react";
+import {useNavigate} from "react-router-dom";
 import axios from "axios";
 import {API_URL} from "./config.js";
 
 export function Profile() {
     const [user, setUser] = useState(null);
     const userId = localStorage.getItem("userId");
+    const navigate = useNavigate();
 
     useEffect(() => {
         async function fetchProfile() {
