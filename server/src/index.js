3b90db79465d465e6cde99051a98d54ccdca3e88
shubh <shubhamwagh2544@gmail.com>
2025-12-03 14:38:56 +0530

    server app

--- server/src/index.js
+++ server/src/index.js
@@ -8,9 +8,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-    origin: [
-        'http://localhost:5173'
-    ],
+    origin: process.env.CLIENT_URL,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
